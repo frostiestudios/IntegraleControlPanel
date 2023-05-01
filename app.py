d89a8b9f333c2e8e224e8a586b0d2b34b9cab02d
thememetsar <mattwiner04@gmail.com>
2023-05-01 11:52:12 -0700

    1.2

--- app.py
+++ app.py
@@ -9,9 +9,28 @@ def index():
 
 @route('/sigma')
 def sigma():
-    return static_file('sigma.html',root='')
+    conn = sqlite3.connect('content.db')
+    c = conn.cursor()
+    c.execute("SELECT id, ip, name FROM computers")
+    result = c.fetchall()
+    c.close()
+    output = template('sigma', rows=result)
+    return output
 
-@route('/servers')
+@route('/sigma/new',method='GET')
+def new():
+    if request.GET.get('save','').strip():
+        ip = request.GET.get('ip','').strip()
+        name = request.GET.get('name','').strip()
+        conn = sqlite3.connect('content.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO computers (ip,name) VALUES (?,?)",(ip,name))
+        conn.commit()
+        c.close()
+        return redirect('/sigma')
+    else:
+        return template("newpc")
+@route('/servers')  
 def servers():
     conn = sqlite3.connect('content.db')
     c = conn.cursor()
@@ -34,4 +53,28 @@ def new():
         return redirect('/servers')
     else:
         return template("new")
+#Content
+#Cars
+@route('/content/cars')
+def content():
+    conn = sqlite3.connect('content.db')
+    c = conn.cursor()
+    c.execute('SELECT id, make, model, year, country FROM cars')
+    result = c.fetchall()
+    c.close()
+    output = template('ccars', rows=result)
+    return output
+@route('/content/cars/new',method='GET')
+def new():
+    if request.GET.get('save','').strip():
+        make = request.GET.get('make','').strip()
+        model = request.GET.get('model','').strip()
+        year = request.GET.get('year','').strip()
+        country = request.GET.get('','').strip()
+        conn = sqlite3.connect('content.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO cars (make,model,year,country) VALUES ()")
+        
+        
+        
 run(host='localhost',port=5150,debug=True,reloader=True)
